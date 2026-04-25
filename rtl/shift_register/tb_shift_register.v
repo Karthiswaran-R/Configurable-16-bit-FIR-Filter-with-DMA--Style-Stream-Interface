0022070050bd8095729be1407231b6cabafb278d
Karthiswaran R <karthi28974@gmail.com>
2026-04-25 14:51:31 +0530

    Update tb_shift_register.v

--- rtl/shift_register/tb_shift_register.v
+++ rtl/shift_register/tb_shift_register.v
@@ -1,18 +1,12 @@
 
 module shift_register_tb;
-
 parameter N = 8;
 parameter WIDTH = 16;
-
-// DUT signals
 reg clk;
 reg rst_n;
 reg shift_en;
 reg signed [WIDTH-1:0] data_in;
-
 wire signed [N*WIDTH-1:0] x_bus;
-
-// Instantiate DUT
 shift_register #(N, WIDTH) dut (
     .clk(clk),
     .rst_n(rst_n),
@@ -20,16 +14,8 @@ shift_register #(N, WIDTH) dut (
     .data_in(data_in),
     .x_bus(x_bus)
 );
-
-//////////////////////////////////////////////////
-// CLOCK
-//////////////////////////////////////////////////
 initial clk = 0;
-always #5 clk = ~clk;   // 100 MHz
-
-//////////////////////////////////////////////////
-// RESET
-//////////////////////////////////////////////////
+always #5 clk = ~clk;
 task reset_dut;
 begin
     rst_n = 0;
@@ -39,10 +25,6 @@ begin
     rst_n = 1;
 end
 endtask
-
-//////////////////////////////////////////////////
-// INPUT DRIVER
-//////////////////////////////////////////////////
 task send_sample(input signed [15:0] sample);
 begin
     @(posedge clk);
@@ -53,12 +35,7 @@ begin
     shift_en = 0;
 end
 endtask
-
-//////////////////////////////////////////////////
-// MONITOR
-//////////////////////////////////////////////////
 integer i;
-
 always @(posedge clk) begin
     $write("T=%0t | x = ", $time);
     for (i=0;i<N;i=i+1) begin
@@ -66,27 +43,16 @@ always @(posedge clk) begin
     end
     $write("\n");
 end
-
-//////////////////////////////////////////////////
-// TEST SEQUENCE
-//////////////////////////////////////////////////
 initial begin
-    $dumpfile("shift_register.vcd");
+    $dumpfile("shift_register.f");
     $dumpvars(0, shift_register_tb);
-
     reset_dut();
-
-    // Apply samples
     send_sample(10);
     send_sample(20);
     send_sample(30);
     send_sample(40);
     send_sample(50);
-
-    // Wait to observe shifts
     repeat(10) @(posedge clk);
-
     $finish;
 end
-
 endmodule
